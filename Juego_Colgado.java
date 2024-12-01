c388fbb6ab82e2032ae193f9123a0bbdf993beae
quimpueyo <quim.pueeyo@gmail.com>
2024-12-01 23:12:24 +0100

    Últimes condicions i acabar de polir el codi
    Poso les ultimes condicions i les comento una mica per sobre

--- Juego_Colgado.java
+++ Juego_Colgado.java
@@ -1,8 +1,8 @@
 package Entregables1;
 
-import java.util.Scanner;  // Importa la classe Scanner per llegir dades des de l'entrada de l'usuari
+import java.util.Scanner; 
 
-public class Juego_Colgado {  // Inici de la classe principal anomenada Main
+public class Juego_Colgado { 
     public static void main(String[] args) {
         // Declarar variables
         int menu = 0; // Variable menu amb un valor de 0 per previnir errors. L'utilitzaré per eligir una opció del switch-case.
@@ -37,7 +37,7 @@ public class Juego_Colgado {  // Inici de la classe principal anomenada Main
                     rondes = sc.nextInt();
 
                     for (int i = 0; i < rondes; i++) {
-                        System.out.println("Partida " + (i + 1));
+                        System.out.println("Partida " + (i + 1)); // Mostrar partida actual
                         jugarPartida(jugadores); // Crido la funció jugar per tenir els menus separats
                     }
 
@@ -60,40 +60,64 @@ public class Juego_Colgado {  // Inici de la classe principal anomenada Main
 
     // Creo una funció
     public static void jugarPartida(int njugadors) {
+        // Declarem variables que volsrem utilitzar
         Scanner sc = new Scanner(System.in);
         int intents = 6;
         String palabra = "";
-        String paraulaAmagada = "";
         String lletra = "";
 
         // Preguntar paraula secreta
-        System.out.println("Introduzca la palabra secreta");
+        System.out.println("Admin: Introduzca la palabra secreta");
         palabra = sc.next();
+
+        // Inicializamos el array de la palabra secreta con la longitud correcta
+        char[] paraulaAmagada = new char[palabra.length()];
+
+        // Mostrar paraula amagada per guions
+        for (int p = 0; p < palabra.length(); p++) {
+            paraulaAmagada[p] = '_';
+        }
+
+        System.out.println("Paraula: " + new String(paraulaAmagada));
 
         // Partida
         for (int i = 0; i < intents; i++) {
-            for (int n = 0; n < njugadors; n++) {
-                if (i == 5) {
-                    System.out.println("Jugador " + (n + 1) + " has perdut");
-                    break;
-                } else {
-                    paraulaAmagada = "";
+            boolean jugadorHaGuanyat = false;  //Per controlar si algun jugador ha guanyat en aquesta ronda.
 
-                    // Mostrar paraula amagada
-                    for (int p = 0; p < palabra.length(); p++)
-                    {
-                        paraulaAmagada = paraulaAmagada + "_";
+            for (int n = 0; n < njugadors; n++) {//bucle per iterar a través de tots els jugadors.
+                if (jugadorHaGuanyat) {// Si un jugador ha guanyat, es surt del bucle i s'acaba la ronda.
+                    break;
+                }
+                // Si s'ha arribat a l'intent 5 (el sisè intent, perquè l'índex comença a 0), el jugador perd.
+                if (i == 5) {
+                    System.out.println("Jugador " + (n + 1) + " has perdut, no tens mé sintents");
+                    break;// Surt del bucle perquè el jugador ha perdut.
+                } else {
+                    if (palabra.equals(new String(paraulaAmagada))) {// Si no és l'intent 5, es comprova si la paraula amagada coincideix amb la paraula secreta.
+                        System.out.println("Jugador " + (n + 1) + " ha trobat la paraula. La paraula secreta era " + palabra);
+                        jugadorHaGuanyat = true;
+                        break;// Surt del bucle, ja que el jugador ha guanyat.
+                    } else {
+                        // Preguntar una paraula (per si no coincideix)
+                        System.out.println("Jugador " + (n + 1) + " dis una lletra");
+                        lletra = sc.next();
+
+                        for (int x = 0; x < palabra.length(); x++) { //bucle per verificar cada lletra de la paraula secreta.
+                            char ara = palabra.charAt(x);//Obté la lletra de la paraula secreta a la posició 'x'.
+
+                            if (Character.toString(ara).equals(lletra)) {// si la lletra introduïda pel jugador coincideix amb la lletra de la paraula secreta, s'actualitza la paraula amagada.
+                                paraulaAmagada[x] = ara;
+                            }
+                        }
                     }
 
-                    System.out.println("Paraula: " + paraulaAmagada);
-
-                    // Preguntar una paraula
-                    System.out.println("Dis una lletra");
-                    lletra = sc.next();
+                    System.out.println("Paraula " + new String(paraulaAmagada));
                 }
+            }
 
+            if (jugadorHaGuanyat) { //si algun jugador ha guanyat, s'acaba el bucle principal de la partida.
+                break;
             }
         }
-
     }
 }
